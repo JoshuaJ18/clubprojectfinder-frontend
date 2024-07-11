11df8a2e1a4f45c25a6a1bdea9192f9ea68a814e
Joshua Jones <109106928+JoshuaJ18@users.noreply.github.com>
2024-07-10 23:01:16 -0700

    Update README.md (#1)
    Signed-off-by: Joshua Jones <109106928+JoshuaJ18@users.noreply.github.com>

--- webapp/src/routes/PageRouter.tsx
+++ webapp/src/routes/PageRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, IndexRouteObject } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 
 function PageRouter() {
